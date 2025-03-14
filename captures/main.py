eaff3966c4c44abc198f87c8fa62df5e27731938
Zouaghi Jihed <zwghijihed@gmail.com>
2025-03-14 09:48:36 +0100

    fusionnement en un seul commit

--- captures/main.py
+++ captures/main.py
@@ -10,5 +10,14 @@ som=somme(my_list)
 print('la somme est =',som)
 
 my_list=[1,5,7]
+<<<<<<< HEAD
 print('la somme est',sum(my_list))
 
+=======
+if my_list:
+	print('la somme est',sum(my_list))
+	print('le max est',max(my_list))
+	print('le min est',min(my_list))
+else:
+	print('liste vide')
+>>>>>>> feature_list
